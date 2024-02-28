8b9cc9cfd90694273e85fd2f260533861acb79b2
Kei-K23 <keiksl2301@gmail.com>
2024-02-28 14:45:05 +0630

    CRUD for products model

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,42 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @ApiProperty({ required: true })
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @ApiProperty({ required: true })
+  description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty({ required: true })
+  price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty({ required: true })
+  stock: number;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ required: false })
+  image?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ required: true })
+  categoryId: string;
+}
